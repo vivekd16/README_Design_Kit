eda19c195f861d6c4bbbfc886506b9c4056d2c76
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-06-12 11:54:48 +0530

    added sidebar UserInput Theme-provider and fixed bug related to sonnar toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { Toaster } from "@/components/ui/sonner";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import { ThemeProvider } from "./components/theme-provider";
+import {Toaster} from './components/ui/sonner'
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
+export default function App(){
+  return(
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
 
 
-          {/* page not found route */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+            {/* page not found route */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        </ThemeProvider>
+      </TooltipProvider>
+      <Toaster richColors />
+    </QueryClientProvider>
+  )
+};
 
-export default App;
